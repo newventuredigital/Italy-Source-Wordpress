f6d4981660542cdfb96ecce209c957258064a200
Michael Roach <mike@roachdesign.com>
2015-08-11 13:33:59 -0400

    Style Tweaks

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
   <div class="row">
     <div class="medium-8 columns">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <h2><?php the_title(); ?></h2>
-      <?php the_content(); ?>
+      <?php $url = get_the_permalink(); ?>
+      <h3><a href="<?php echo $url; ?>"><?php the_title(); ?></a></h3>
+      <p><?php echo excerpt(40); ?></p>
       <p><?php edit_post_link(); ?></p>
       <hr>
       <?php endwhile; endif; ?>   
