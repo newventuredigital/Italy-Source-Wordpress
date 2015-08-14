11a9f89fbeaf3f718c9f79e6b97789d1ac439ce5
Michael Roach <mike@roachdesign.com>
2015-08-14 11:51:21 -0400

    Static & Post Featured Images

--- single.php
+++ single.php
@@ -3,8 +3,8 @@
   <div class="row">
     <div class="medium-8 columns">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <?php $url = get_the_permalink(); ?>
-      <h3><a href="<?php echo $url; ?>"><?php the_title(); ?></a></h3>
+      <h3><?php the_title(); ?></h3>
+      <?php if (has_post_thumbnail()) {the_post_thumbnail('large', array( 'class' => 'content-featured-image' )); );} ?>
       <p><?php the_content(); ?></p>
       <p><?php edit_post_link(); ?></p>
       <hr>
